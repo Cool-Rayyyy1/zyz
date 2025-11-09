1a07447a31c1a3cf5e4b0513783d8d12c707e0cc
Cool-Rayyyy1 <Cool-Rayyyy1@users.noreply.github.com>
2025-11-09 23:11:21 +0000

    deploy: f6967002a1999c332c622e5faf221a276ea1747f

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Cool-Rayyyy1/zyz",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
